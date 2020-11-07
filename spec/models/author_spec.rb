136b1577aa5223e9735d265e1e124ab7ba0952b2
sirkrypt0 <tobiaskantusch@online.de>
2020-11-07 14:45:49 +0100

    Use factory in tests for #3

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,25 +1,24 @@
 require 'rails_helper'
 
-describe "Author model", type: :model do
+describe "Author", type: :model do
 
   it "should have a first_name attribute" do
-    author = Author.new('Alan', 'Turing', 'http://wikipedia.org/Alan_Turing')
+    author = create(:author) # use factory to create author
     expect(author.first_name).to eq('Alan')
   end
 
   it "should have a last_name attribute" do
-    author = Author.new('Alan', 'Turing', 'http://wikipedia.org/Alan_Turing')
+    author = create(:author)
     expect(author.last_name).to eq('Turing')
   end
 
   it "should have a homepage attribute" do
-    author = Author.new('Alan', 'Turing', 'http://wikipedia.org/Alan_Turing')
+    author = create(:author)
     expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
   end
 
   it "should have a method name which returns the full name of an author" do
-    author = Author.new('Alan', 'Turing', 'http://wikipedia.org/Alan_Turing')
+    author = create(:author)
     expect(author.name).to eq('Alan Turing')
   end
-
 end
