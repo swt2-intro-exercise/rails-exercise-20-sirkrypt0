919fd3370b6dfb56a51612fd75d59a937f5a6e5f
sirkrypt0 <tobiaskantusch@online.de>
2020-11-25 14:23:01 +0100

    Add failing tests for #10

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 describe "Index author page", type: :feature do
 
+  before :all do
+    create(:author)
+    @author1 = Author.new first_name: 'Bill', last_name: 'Gates', homepage: 'http://wikipedia.org/Bill_Gates'
+  end
+
   it "should render without error" do
     visit authors_path
   end
@@ -20,16 +25,16 @@ describe "Index author page", type: :feature do
     end
   end
 
+  it "should contain a link to add new authors" do
+    visit authors_path
+    expect(page).to have_link 'New', href: new_author_path
+  end
+
   it "should display a table containing a link to edit an author" do
     visit authors_path
     Author.all.each do |author|
       expect(page).to have_link("edit", href: edit_author_path(author))
     end
-  end
-
-  it "should contain a link to add new authors" do
-    visit authors_path
-    expect(page).to have_link 'New', href: new_author_path
   end
 
 end
