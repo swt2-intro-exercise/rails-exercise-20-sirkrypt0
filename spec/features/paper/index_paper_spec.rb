e9bc7ddaf42bffa8342eb9c18cb1a1d05787607e
sirkrypt0 <tobiaskantusch@online.de>
2020-11-25 17:26:49 +0100

    Fix test for #17

--- spec/features/paper/index_paper_spec.rb
+++ spec/features/paper/index_paper_spec.rb
@@ -4,7 +4,9 @@ describe "Index author page", type: :feature do
 
   before :all do
     @paper1 = create(:paper)
+    @paper1.save
     @paper2 = Paper.new title: 'Paper 2', venue: 'HPI', year: 1337
+    @paper2.save
   end
 
   it "should show papers from year only when filtering with year" do
