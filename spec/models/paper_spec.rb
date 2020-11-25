4339403281b33b3c00eaf52c9fbb4283f50cc623
sirkrypt0 <tobiaskantusch@online.de>
2020-11-25 15:18:04 +0100

    Add failing tests for #13

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should not be valid without any attribute" do
+    paper = Paper.new({})
+    expect(paper).to_not be_valid
+  end
+
+  it "should not be valid without title" do
+    paper = Paper.new venue: 'HPI', year: 2000
+    expect(paper).to_not be_valid
+  end
+
+  it "should not be valid without venue" do
+    paper = Paper.new title: 'Paper', year: 2000
+    expect(paper).to_not be_valid
+  end
+
+  it "should not be valid without year" do
+    paper = Paper.new title: 'Paper', venue: 'HPI'
+    expect(paper).to_not be_valid
+  end
+
+  it "should not be valid with year being not numeric" do
+    paper = Paper.new title: 'Paper', venue: 'HPI', year: '2019'
+    expect(paper).to_not be_valid
+  end
+
 end
